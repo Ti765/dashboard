577a906a05892fdc245674b9bd2b4b45a86273d4
Ti765 <161473411+Ti765@users.noreply.github.com>
2025-06-06 13:55:58 -0300

    Revamp UI with dark theme and new components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,11 @@
-import * as React from "react"
+import clsx from "clsx";
 
-import { cn } from "@/lib/utils"
-
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-base leading-normal ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
-
-export { Input }
+export const Input = ({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>) => (
+  <input
+    className={clsx(
+      "w-full rounded-md bg-bg-faint px-3 py-2 text-fg placeholder:opacity-50 focus:ring-2 focus:ring-accent",
+      className
+    )}
+    {...props}
+  />
+);
